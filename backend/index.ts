7eec3f287fbd546fa94ef1055f333c5c92f5b0cd
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-02-15 15:39:03 +0530

    Backend-Setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import { ENV } from './src/config/env.js';
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+    origin: ENV.FRONTEND_URL, // Allow all origins
+})); // Enable CORS for all routes
+app.use(clerkMiddleware()); // Add Clerk middleware to handle authentication and user management
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -8,4 +8,7 @@ export const ENV = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV ,
     DB_URL: process.env.DB_URL,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    FRONTEND_URL: process.env.FRONTEND_URL,
 };
