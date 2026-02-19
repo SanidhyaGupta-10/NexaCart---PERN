5149a2faac53a6f8bb0933d63b5dd6f73899f13e
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-02-19 20:30:04 +0530

    Completed Home-Page and Product-Create-Page

--- web/app/src/hooks/useProducts.ts
+++ web/app/src/hooks/useProducts.ts
@@ -1,9 +1,13 @@
 "use client"
 
-import { useQuery } from "@tanstack/react-query"
-import { getAllProducts } from "../lib/api"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { createProduct, getAllProducts } from "../lib/api"
 
 export const useProducts = () => {
     const result = useQuery({ queryKey: ["products"], queryFn: getAllProducts });
     return result;
+}
+
+export const useCreateProduct = () => {
+    return useMutation({ mutationFn: createProduct });
 }
